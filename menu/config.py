6a73cd7565ecbf88da3a7b7631f34939b25b7508
Romain VORLET <romain.vorlet@gmail.com>
2023-11-14 00:10:20 +0100

    bouton retour fonction ok

--- menu/config.py
+++ menu/config.py
@@ -1,3 +1,7 @@
+import pygame
+
+from score.score_display import menu
+
 largeur_ecran = 1280
 hauteur_ecran = 720
 
@@ -8,3 +12,7 @@ def get_largeur_ecran():
 
 def get_hauteur_ecran():
     return hauteur_ecran
+
+
+pygame.init()
+menu(largeur_ecran, hauteur_ecran)
